ab51b9bf4f04e3d4112bfc6f7aa74cd55a674f76
Shamal Jayawardhana <54359810+shamalja@users.noreply.github.com>
2025-01-16 20:37:25 +0530

    Create App.js

---
+++ content/blogs/react-file-download/demo/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FileDownload from './FileDownload';
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', marginTop: '20px' }}>
+        File Download in React
+      </h1>
+      <FileDownload />
+    </div>
+  );
+};
+
+export default App;

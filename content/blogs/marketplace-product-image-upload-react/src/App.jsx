6a471d2e00c799c933c26363f5b61f868b9979b1
Shamal Jayawardhana <54359810+shamalja@users.noreply.github.com>
2025-10-31 19:32:01 +0530

    Create App.jsx

---
+++ content/blogs/marketplace-product-image-upload-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductImageUploader from './ProductImageUploader';
+
+function App() {
+  return (
+    <div>
+      <ProductImageUploader />
+    </div>
+  );
+}
+
+export default App;

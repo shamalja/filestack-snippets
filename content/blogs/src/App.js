1974c5d06e9fe72fca340f5eacb30814fc66c008
Shamal Jayawardhana <54359810+shamalja@users.noreply.github.com>
2025-11-04 01:52:57 +0530

    Create App.js

---
+++ content/blogs/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResumeUpload from './ResumeUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeUpload />
+    </div>
+  );
+}
+
+export default App;
